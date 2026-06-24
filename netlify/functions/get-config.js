eb41fa4749227260d4373213d34ea9792811933a
Ishant Batra <ishantpbatra1234@gmail.com>
2026-06-23 18:09:27 -0600

    gemini

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -6,12 +6,12 @@ exports.handler = async () => {
     },
     body: JSON.stringify({
       apiKey: process.env.PROCESS_API_KEY,
-      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-      storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      appId: import.meta.env.VITE_FIREBASE_APP_ID,
-      tbaKey: import.meta.env.VITE_TBA_AUTH_KEY
+      authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VITE_FIREBASE_APP_ID,
+      tbaKey: process.env.VITE_TBA_AUTH_KEY
     }),
   };
 };
